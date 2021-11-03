5d4717847b5d27f9805ac06213b6b60a70d75bed
Wagner Andrade <wagnerandrade_df@hotmail.com>
2021-11-02 22:25:01 -0300

    Primeiro commit

---
+++ src/main/java/com/estudos/cobranca/CobrancaApplication.java
@@ -0,0 +1,14 @@
+package com.estudos.cobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobrancaApplication.class, args);
+	}
+
+}
+ 
---
+++ src/main/java/com/estudos/cobranca/controle/TituloController.java
@@ -0,0 +1,16 @@
+package com.estudos.cobranca.controle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TituloController {
+	
+	@RequestMapping("/titulos/novo")
+	public String novo() {
+		return "CadastroTitulo";
+	}
+	
+
+}
+ 
