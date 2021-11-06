0abd02a39aeaad5a837d5ac826297a832716d23a
Wagner Andrade <wagnerandrade_df@hotmail.com>
2021-11-05 21:09:54 -0300

    Enviando dados Form por POST para o servidor

--- src/main/java/com/estudos/cobranca/controle/TituloController.java
+++ src/main/java/com/estudos/cobranca/controle/TituloController.java
@@ -2,15 +2,26 @@ package com.estudos.cobranca.controle;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.estudos.cobranca.model.Titulo;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 	
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/novo")
 	public String novo() {
 		return "CadastroTitulo";
 	}
 	
-
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		//Salvar no banco de dados
+		System.out.println(">>>> "+titulo.getDescricao());
+		
+		return "CadastroTitulo";
+	}
+	
 }
  
